92289282d24441f868569f9a7d900352e12259e9
veera-yerramsetty-usa <veera.y2025@gmail.com>
2026-02-16 11:31:29 -0800

    project initiated

---
+++ src/main/java/org/sample/simpleenterprizeproj2/SimpleEnterprizeProj2Application.java
@@ -0,0 +1,13 @@
+package org.sample.simpleenterprizeproj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEnterprizeProj2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleEnterprizeProj2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/sample/simpleenterprizeproj2/SimpleEnterprizeProj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sample.simpleenterprizeproj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEnterprizeProj2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
